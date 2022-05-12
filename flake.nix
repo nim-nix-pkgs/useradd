6d4eec72605e7df7709d6be3cee8e4e94682f2fe
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:35 -0300

    chore: re index useradd

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."useradd-master".dir   = "master";
+  inputs."useradd-master".owner = "nim-nix-pkgs";
+  inputs."useradd-master".ref   = "master";
+  inputs."useradd-master".repo  = "useradd";
+  inputs."useradd-master".type  = "github";
+  inputs."useradd-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."useradd-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."useradd-0_1_0".dir   = "0_1_0";
+  inputs."useradd-0_1_0".owner = "nim-nix-pkgs";
+  inputs."useradd-0_1_0".ref   = "master";
+  inputs."useradd-0_1_0".repo  = "useradd";
+  inputs."useradd-0_1_0".type  = "github";
+  inputs."useradd-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."useradd-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
